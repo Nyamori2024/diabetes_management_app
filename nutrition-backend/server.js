b35dd61bc1a44463685a2e7f0a1adc9f20bfa64d
Christian Mathews Abuto Fullstack/Backend /Frontend Developer <128970224+Nyamori2024@users.noreply.github.com>
2025-03-03 15:58:31 +0000

    updated server.js

--- nutrition-backend/server.js
+++ nutrition-backend/server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 
 app.use('/api/measurements', measurementRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT
 
 // Ensure the database connects before starting the server
 connectDB().then(() => {
